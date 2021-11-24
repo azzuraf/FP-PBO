8a4152e263bd5cb5195c6d5b794e962b54d29d31
Selfira Ayu Sheehan <80016547+selfiraa@users.noreply.github.com>
2021-11-24 22:42:10 +0700

    Add files via upload

---
+++ Tetris/src/tetris/GameArea.java
@@ -0,0 +1,44 @@
+package tetris;
+
+import javax.swing.JPanel;
+import java.awt.Color;
+import java.awt.Graphics;
+        
+public class GameArea extends JPanel {
+    private int gridRows;
+    private int gridColumns;
+    private int gridCellSize;
+    
+    private int[][] block = {{1,0},{1,0},{1,1}};
+    
+    public GameArea(JPanel placeholder, int columns) {
+        placeholder.setVisible(false);
+        this.setBounds (placeholder.getBounds());
+        this.setBackground (placeholder.getBackground());
+        this.setBorder (placeholder.getBorder());
+        
+        gridColumns = columns;
+        gridCellSize = this.getBounds().width / gridColumns;
+        gridRows = this.getBounds().height / gridCellSize;
+    }
+    
+    private void drawBlock(Graphics g) {
+        for (int row = 0; row < block.length; row++) {
+            for (int col = 0; col < block[0].length; col++) {
+                if (block[row][col] == 1) {
+                    g.setColor(Color.red);
+                    g.fillRect (col * gridCellSize, row * gridCellSize, gridCellSize, gridCellSize);
+                    g.setColor(Color.black);
+                    g.drawRect(col * gridCellSize, row * gridCellSize, gridCellSize, gridCellSize);
+                }
+            }
+        }
+    }
+    
+    @Override
+    protected void paintComponent (Graphics g) {
+        super.paintComponent(g);
+        
+        drawBlock (g);
+    }
+}
---
+++ Tetris/src/tetris/GameForm.java
@@ -0,0 +1,88 @@
+package tetris;
+
+public class GameForm extends javax.swing.JFrame {
+    public GameForm() {
+        initComponents();
+        this.add (new GameArea(gameAreaPlaceholder, 10));
+    }
+
+    @SuppressWarnings("unchecked")
+    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
+    private void initComponents() {
+
+        gameAreaPlaceholder = new javax.swing.JPanel();
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+        setResizable(false);
+
+        gameAreaPlaceholder.setBackground(new java.awt.Color(238, 238, 238));
+        gameAreaPlaceholder.setBorder(javax.swing.BorderFactory.createLineBorder(null));
+
+        javax.swing.GroupLayout gameAreaPlaceholderLayout = new javax.swing.GroupLayout(gameAreaPlaceholder);
+        gameAreaPlaceholder.setLayout(gameAreaPlaceholderLayout);
+        gameAreaPlaceholderLayout.setHorizontalGroup(
+            gameAreaPlaceholderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 198, Short.MAX_VALUE)
+        );
+        gameAreaPlaceholderLayout.setVerticalGroup(
+            gameAreaPlaceholderLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 298, Short.MAX_VALUE)
+        );
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addGap(150, 150, 150)
+                .addComponent(gameAreaPlaceholder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(150, 150, 150))
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(layout.createSequentialGroup()
+                .addContainerGap()
+                .addComponent(gameAreaPlaceholder, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+        );
+
+        pack();
+        setLocationRelativeTo(null);
+    }// </editor-fold>//GEN-END:initComponents
+
+    public static void main(String args[]) {
+        /* Set the Nimbus look and feel */
+        //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
+        /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
+         * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html 
+         */
+        try {
+            for (javax.swing.UIManager.LookAndFeelInfo info : javax.swing.UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    javax.swing.UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (ClassNotFoundException ex) {
+            java.util.logging.Logger.getLogger(GameForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (InstantiationException ex) {
+            java.util.logging.Logger.getLogger(GameForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (IllegalAccessException ex) {
+            java.util.logging.Logger.getLogger(GameForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        } catch (javax.swing.UnsupportedLookAndFeelException ex) {
+            java.util.logging.Logger.getLogger(GameForm.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+        }
+        //</editor-fold>
+
+        /* Create and display the form */
+        java.awt.EventQueue.invokeLater(new Runnable() {
+            public void run() {
+                new GameForm().setVisible(true);
+            }
+        });
+    }
+
+    // Variables declaration - do not modify//GEN-BEGIN:variables
+    private javax.swing.JPanel gameAreaPlaceholder;
+    // End of variables declaration//GEN-END:variables
+}
