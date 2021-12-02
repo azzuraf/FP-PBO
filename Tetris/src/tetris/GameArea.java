8ec95c9f3f37433a8ec78fcd116d1c655c84c700
Adinda Zahra Pamuji <89954689+adindazhr@users.noreply.github.com>
2021-12-02 23:57:46 +0700

    Add files via upload

--- Tetris/src/tetris/GameArea.java
+++ Tetris/src/tetris/GameArea.java
@@ -8,6 +8,7 @@ public class GameArea extends JPanel {
     private int gridRows;
     private int gridColumns;
     private int gridCellSize;
+    private Color[][] background;
     
     private TetrisBlock block;
     
@@ -21,7 +22,7 @@ public class GameArea extends JPanel {
         gridCellSize = this.getBounds().width / gridColumns;
         gridRows = this.getBounds().height / gridCellSize;
         
-        spawnBlock();
+        background = new Color[gridRows][gridColumns];
     }
     
     public void spawnBlock()
@@ -30,14 +31,81 @@ public class GameArea extends JPanel {
         block.spawn(gridColumns);
     }
     
-    public void moveBlockDown()
+    public boolean isBlockOutOfBounds()
     {
+        //if the top row is not in the game area
+        //then it is out of bound
+        
+        if(block.getY() < 0)
+        {
+            block = null;
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+    
+    public boolean moveBlockDown() {
+        
         if(checkBottom() == false)
         {
-            return;
+            return false;
         }
         
         block.moveDown();
+        repaint();
+        
+        return true;
+    }
+    
+    public void moveBlockRight() {
+        
+        if(block == null)
+        {
+            return;
+        }
+        if(checkRight() == false)
+            return;
+        
+        block.moveRight();
+        repaint();
+    }
+    
+    public void moveBlockLeft() {
+        
+        if(block == null)
+        {
+            return;
+        }
+        if(checkLeft() == false)
+            return;
+        
+        block.moveLeft();
+        repaint();
+    }
+    
+    public void dropBlock() {
+        
+        if(block == null)
+        {
+            return;
+        }
+        while (checkBottom()) {
+            block.moveDown();
+        }
+        
+        repaint();
+    }
+    
+    public void rotateBlock() {
+        
+        if(block == null)
+        {
+            return;
+        }
+        block.rotate();
         repaint();
     }
     
@@ -47,9 +115,149 @@ public class GameArea extends JPanel {
         {
             return false;
         }
-        else
+        
+        int[][]shape = block.getShape();
+        int w = block.getWidth();
+        int h = block.getHeight();
+        
+        for(int col = 0; col < w; col++){
+            for(int row = h - 1; row >= 0; row--){
+                if(shape[row][col] != 0) {
+                    int x = col + block.getX();
+                    int y = row + block.getY() + 1;
+                    if (y < 0) break;
+                    if(background[y][x] != null)
+                        return false;
+                    break;
+                }
+            }
+        }
+        
+        return true;
+        
+    }
+    
+    private boolean checkRight() {
+        if(block.getRightEdge() == gridColumns) return false;
+        
+        int[][]shape = block.getShape();
+        int w = block.getWidth();
+        int h = block.getHeight();
+        
+        for(int row = 0; row < h; row++){
+            for(int col = w - 1; col >= 0; col--) {
+                if(shape[row][col] != 0) {
+                    int x = col + block.getX() + 1;
+                    int y = row + block.getY();
+                    if (y < 0) break;
+                    if(background[y][x] != null)
+                        return false;
+                    break;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    private boolean checkLeft() {
+        if(block.getLeftEdge() == 0) return false;
+        
+        int[][]shape = block.getShape();
+        int w = block.getWidth();
+        int h = block.getHeight();
+        
+        for(int row = 0; row < h; row++){
+            for(int col = 0; col < w; col++) {
+                if(shape[row][col] != 0) {
+                    int x = col + block.getX() - 1;
+                    int y = row + block.getY();
+                    if (y < 0) break;
+                    if(background[y][x] != null)
+                        return false;
+                    break;
+                }
+            }
+        }
+        
+        return true;
+    }
+    
+    public int clearLines()
+    {
+        boolean lineFilled;
+        int linesCleared = 0;
+        
+        //find completed lines
+        for(int r = gridRows - 1; r >= 0; r--)
         {
-            return true;
+            lineFilled = true;
+            
+            //to check whether line is filled or not
+            for(int c = 0; c < gridColumns; c++)
+            {
+                //line is not completed
+                //and must be ignored
+                if(background[r][c] ==  null)
+                {
+                    lineFilled = false;
+                    break;
+                }
+            }
+            
+            //if line is filled then block will be cleared
+            if(lineFilled == true)
+            {
+                linesCleared++;
+                clearLine(r);
+                shiftDown(r);
+                clearLine(0); //clear top line
+                
+                r++;//when there is two lines that's filled
+                //so it doesn't get skipped
+                
+                repaint();
+            }
+        }
+        return linesCleared;
+    }
+    
+    private void clearLine(int r)
+    {
+        for(int i = 0; i < gridColumns; i++)
+        {
+            background[r][i]=null;
+        }
+    }
+    
+    private void shiftDown(int r)
+    {
+        for(int row = r; row > 0; row--)
+        {
+            for(int col = 0; col < gridColumns; col++)
+            {
+                //replace current line with line above it
+                background[row][col] = background[row-1][col];
+            }
+        }
+    }
+    
+    public void moveBlockToBackground() {
+        int[][] shape = block.getShape();
+        int h = block.getHeight();
+        int w = block.getWidth();
+        
+        int xPos = block.getX();
+        int yPos = block.getY();
+        
+        Color color = block.getColor();
+        
+        for (int r = 0; r < h; r++) {
+            for (int c = 0; c < w; c++) {
+                if (shape[r][c] == 1) {
+                    background[r + yPos][c + xPos] = color;
+                }
+            }
         }
     }
     
@@ -66,19 +274,41 @@ public class GameArea extends JPanel {
                     int x = (block.getX()+col) * gridCellSize;
                     int y = (block.getY()+row) * gridCellSize;
                     
-                    g.setColor(c);
-                    g.fillRect (x, y, gridCellSize, gridCellSize);
-                    g.setColor(Color.black);
-                    g.drawRect(x, y, gridCellSize, gridCellSize);
+                    drawGridSquare(g, c, x, y);
                 }
             }
         }
+    }
+    
+    private void drawBackground(Graphics g) {
+        Color color;
+                
+        for (int r = 0; r < gridRows; r++) {
+            for (int c = 0; c < gridColumns; c++) {
+                color = background[r][c];
+                
+                if (color != null) {
+                    int x = c * gridCellSize;
+                    int y = r * gridCellSize;
+                    
+                    drawGridSquare (g, color, x, y);
+                }
+            }
+        }
+    }
+    
+    private void drawGridSquare(Graphics g, Color color, int x, int y) {
+        g.setColor(color);
+        g.fillRect (x, y, gridCellSize, gridCellSize);
+        g.setColor(Color.black);
+        g.drawRect(x, y, gridCellSize, gridCellSize);
     }
     
     @Override
     protected void paintComponent (Graphics g) {
         super.paintComponent(g);
         
+        drawBackground(g);
         drawBlock (g);
     }
 }
--- Tetris/src/tetris/TetrisBlock.java
+++ Tetris/src/tetris/TetrisBlock.java
@@ -11,16 +11,42 @@ public class TetrisBlock
     private int[][] shape;
     private Color color;
     private int x,y;
+    private int[][][] shapes;
+    private int currentRotation;
     
     //constructor
     public TetrisBlock(int[][] shape, Color color)
     {
         this.shape = shape;
         this.color = color;
+        
+        initShapes();
+    }
+    
+    private void initShapes() {
+        shapes = new int[4][][];
+        
+        for (int i = 0; i < 4; i++) {
+            int r = shape[0].length;
+            int c = shape.length;
+            
+            shapes[i] = new int [r][c];
+            
+            for (int y = 0; y < r; y++) {
+                for (int x = 0; x < c; x++) {
+                    shapes[i][y][x] = shape[c - x - 1][y];
+                }
+            }
+            
+            shape = shapes[i];
+        }
     }
     
     public void spawn(int gridWidth)
     {
+        currentRotation = 0;
+        shape = shapes[currentRotation];
+        
         y = 0-getHeight();
         x = (gridWidth - getWidth())/2;
     }
@@ -70,8 +96,21 @@ public class TetrisBlock
         x--;
     }
     
+    public void rotate() {
+        currentRotation++;
+        if (currentRotation > 3) currentRotation = 0;
+        shape = shapes[currentRotation];
+    }
     public int getBottomEdge()
     {
         return (y+getHeight());
+    }
+    
+    public int getRightEdge() {
+        return x + getWidth();
+    }
+    
+    public int getLeftEdge() {
+        return x;
     }
 }
